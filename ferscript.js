9cc2aaa28574d425d6a31c255562218f6efebf88
Fer <57419948+dibesfer@users.noreply.github.com>
2021-03-29 23:48:20 +0200

    Update ferscript.js

--- ferscript.js
+++ ferscript.js
@@ -6,3 +6,11 @@ function getid(s){
 function write(where,what){
         where.innerHTML += what;
 }
+
+function clear(where){
+        where.innerHTML = "";
+}
+
+function randomint(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
