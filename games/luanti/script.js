e390c4fcb3efb3f0af2d5f1829d72373d953fdcb
dibesfer <dibesfer@gmail.com>
2025-02-27 00:38:54 +0100

    itsyou

--- games/luanti/script.js
+++ games/luanti/script.js
@@ -1,1 +1,10 @@
-/* NOTHING YET */
+let counter = 0
+let itsme = document.getElementById("itsme")
+
+itsme.onclick = function(){
+    counter++
+    if (counter > 20){
+        alert("okay its you")
+        localStorage.setItem("luantiguideLocalVisits", -999999999)
+    }
+}
