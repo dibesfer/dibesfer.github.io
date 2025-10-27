7b1da6a0fdc0fc53e22ee737c7cf8f353989ba4e
dibesfer <dibesfer@gmail.com>
2025-10-27 09:41:54 +0100

    loadModules

---
+++ assets/scripts/loadModules.js
@@ -0,0 +1,31 @@
+// requires /ferscript.js
+// it takes header and footer and fills them with html content
+let header = gettag("header")
+let footer = gettag("footer")
+console.log(header.innerHTML)
+function insertHTML() {
+
+    fetch('/assets/modules/header.html')
+        .then(function (response) {
+            // When the page is loaded convert it to text
+            return response.text()
+        })
+        .then(function (html) {
+            header.innerHTML = html;
+            fetch('/assets/modules/footer.html')
+                .then(function (response) {
+                    // When the page is loaded convert it to text
+                    return response.text()
+                })
+                .then(function (html) {
+                    footer.innerHTML = html
+                })
+                .catch(function (err) {
+                    console.log('Failed to fetch page: ', err);
+                });
+        })
+        .catch(function (err) {
+            console.log('Failed to fetch page: ', err);
+        });
+}
+insertHTML()
--- ferscript.js
+++ ferscript.js
@@ -1,3 +1,7 @@
 function getid(id){
     return document.getElementById(id)
 }
+
+function gettag(tag){
+    return document.getElementsByTagName(tag)[0]
+}
