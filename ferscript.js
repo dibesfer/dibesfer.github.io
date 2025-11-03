d83a23fc36d3f5777948323475bc35a7678f8005
dibesfer <dibesfer@gmail.com>
2025-11-03 09:02:21 +0100

    ferscript

--- ferscript.js
+++ ferscript.js
@@ -1,7 +1,38 @@
-function getid(id){
-    return document.getElementById(id)
+function getid(id) {
+  return document.getElementById(id)
 }
 
-function gettag(tag){
-    return document.getElementsByTagName(tag)[0]
+function getclass(class) {
+  return document.getElementsByClassName(class)
+}
+
+function gettag(tag) {
+  return document.getElementsByTagName(tag)[0]
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+let typeWriteCount = 0
+
+function typeWrite(what, where, speed) {
+
+  if (!speed) speed = 75
+
+  let interval = setInterval(type, speed)
+
+  function type() {
+    if (typeWriteCount < what.length) {
+      if (where) where.textContent += what[typeWriteCount]
+      else console.log(what[typeWriteCount])
+      typeWriteCount++;
+    }
+    else clearInterval(interval)
+  }
+
 }
