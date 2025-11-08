1f6328438b37731e6c4a6038f771787c0f48e6f9
dibesfer <dibesfer@gmail.com>
2025-11-08 17:20:11 +0100

    flg

--- web/FakeLanguage/script.js
+++ web/FakeLanguage/script.js
@@ -1,8 +1,14 @@
+// let vowels = ["a", "e", "i", "o", "u","a", "e", "i", "o", "u","a", "e", "i", "o", "u", "ae"]
+// let consonants = ["b", "c", "d", "f", "g", "h", "l", "m", "n", "p", "r", "s", "t", "v", "x", "z", "þ", "ch", "sh",]
+
 let vowels = ["a", "e", "i", "o", "u","a", "e", "i", "o", "u","a", "e", "i", "o", "u", "ae"]
-let consonants = ["b", "c", "d", "f", "g", "h", "l", "m", "n", "p", "r", "s", "t", "v", "x", "z", "þ", "ch", "sh",]
+let consonants = ["b", "c", "d", "f", "g", "h", "l", "m", "n", "p", "r", "s", "t", "v", "x", "z", "ch", "sh",]
+
 let textConsole = getid("textConsole")
-let words = 100
+let words = 17
 let mySentence = ""
+function fillASentence(){
+    mySentence = ""
 for (let i = 0; i < words; i++) {
     let wordlength = randomInt(1, 3)
     let boleano = randomInt(0, 1);
@@ -21,8 +27,17 @@ for (let i = 0; i < words; i++) {
         
     }
     if (i < words-1) mySentence+=" "
-    else mySentence+="."
+    else{
+        mySentence+=".<br><br>"
+        mySentence = capitalizeFirstLetter(mySentence)
+        textConsole.innerHTML += mySentence
+    } 
 }
-
-
-textConsole.innerText = mySentence
+}
+fillASentence()
+fillASentence()
+fillASentence()
+// https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+function capitalizeFirstLetter(val) {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
