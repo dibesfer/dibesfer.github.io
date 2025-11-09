0a32488f4317381ebb917b0848eb00a14e6b9c69
dibesfer <dibesfer@gmail.com>
2025-11-09 10:04:36 +0100

    flg

--- web/FakeLanguage/script.js
+++ web/FakeLanguage/script.js
@@ -1,38 +1,53 @@
 // let vowels = ["a", "e", "i", "o", "u","a", "e", "i", "o", "u","a", "e", "i", "o", "u", "ae"]
 // let consonants = ["b", "c", "d", "f", "g", "h", "l", "m", "n", "p", "r", "s", "t", "v", "x", "z", "þ", "ch", "sh",]
 
-let vowels = ["a", "e", "i", "o", "u","a", "e", "i", "o", "u","a", "e", "i", "o", "u", "ae"]
-let consonants = ["b", "c", "d", "f", "g", "h", "l", "m", "n", "p", "r", "s", "t", "v", "x", "z", "ch", "sh",]
+let vowels = ["a", "e", "i", "o", "u", "a", "e", "i", "o", "u", "a", "e", "i", "o", "u", "ae"]
+let consonants = ["b", "c", "d", "f", "g", "h", "l", "m", "n", "p", "r", "s", "t", "v", "x", "z", "ch", "sh", "y"]
 
 let textConsole = getid("textConsole")
 let words = 17
 let mySentence = ""
-function fillASentence(){
+
+function fillASentence() {
     mySentence = ""
-for (let i = 0; i < words; i++) {
-    let wordlength = randomInt(1, 3)
-    let boleano = randomInt(0, 1);
+    for (let i = 0; i < words; i++) {
+        let wordlength = randomInt(1, 3)
+        let probabilidad = randomInt(1, 3)
 
-    for (let j = 0; j < wordlength; j++) {
-        if (boleano) {
-            mySentence+=vowels[randomInt(0,vowels.length-1)]
-            mySentence+=consonants[randomInt(0,consonants.length-1)]
+        for (let j = 0; j < wordlength; j++) {
+
+            switch (probabilidad) {
+                case 1:
+                    if (wordlength == 1)
+                    mySentence += vowels[randomInt(0, vowels.length - 1)]
+                    else {
+                        mySentence += vowels[randomInt(0, vowels.length - 1)]
+                        mySentence += consonants[randomInt(0, consonants.length - 1)]
+                    }
+                    break;
+
+                case 2:
+                    mySentence += vowels[randomInt(0, vowels.length - 1)]
+                    mySentence += consonants[randomInt(0, consonants.length - 1)]
+                    break;
+
+                case 3:
+                    mySentence += consonants[randomInt(0, consonants.length - 1)]
+                    mySentence += vowels[randomInt(0, vowels.length - 1)]
+                    break;
+
+                default:
+                    break;
+            }
 
         }
+        if (i < words - 1) mySentence += " "
         else {
-            mySentence+=consonants[randomInt(0,consonants.length-1)]
-            mySentence+=vowels[randomInt(0,vowels.length-1)]
-
+            mySentence += ".<br><br>"
+            mySentence = capitalizeFirstLetter(mySentence)
+            textConsole.innerHTML += mySentence
         }
-        
     }
-    if (i < words-1) mySentence+=" "
-    else{
-        mySentence+=".<br><br>"
-        mySentence = capitalizeFirstLetter(mySentence)
-        textConsole.innerHTML += mySentence
-    } 
-}
 }
 fillASentence()
 fillASentence()
