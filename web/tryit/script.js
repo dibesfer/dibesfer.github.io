72f6b5e1ab1616cdedb16820fe9db11fb406425c
dibesfer <dibesfer@gmail.com>
2025-11-11 11:08:39 +0100

    ti no tb

--- web/tryit/script.js
+++ web/tryit/script.js
@@ -50,3 +50,17 @@ function save() {
     saveLink.setAttribute("download", fileName);
     //console.log("save")
 }
+
+function handleKeyDown(e) {
+  if (e.key == "Tab") {
+    e.preventDefault();
+
+    const start = this.selectionStart;
+    const end = this.selectionEnd;
+
+    this.value =
+      this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+    this.selectionStart = this.selectionEnd = start + 1;
+  }
+}
