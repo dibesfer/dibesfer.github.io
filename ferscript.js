3260c7c954a2971c5db1374704b4a81245d6da9d
dibesfer <dibesfer@gmail.com>
2025-12-14 09:58:33 +0100

    t

--- ferscript.js
+++ ferscript.js
@@ -14,6 +14,10 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function randomFromArray(array){
+  return array[randomInt(0,array.length-1)]
+}
+
 function shuffle(array) {
   return array.sort(() => Math.random() - 0.5);
 }
