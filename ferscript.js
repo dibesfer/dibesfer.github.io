ec0d1e28756a0a7c024ba0ee1372016aed334efd
dibesfer <dibesfer@gmail.com>
2026-02-10 22:05:10 +0100

    lusupmnin

--- ferscript.js
+++ ferscript.js
@@ -14,8 +14,8 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomFromArray(array){
-  return array[randomInt(0,array.length-1)]
+function randomFromArray(array) {
+  return array[randomInt(0, array.length - 1)]
 }
 
 function shuffle(array) {
@@ -23,16 +23,16 @@ function shuffle(array) {
 }
 
 function needsazero(number) {
-    let result
+  let result
 
-    if (number < 10) {
-        result = "0" + number
-        return result
-    }
-    return number
+  if (number < 10) {
+    result = "0" + number
+    return result
+  }
+  return number
 }
 
-function decimalTwo(num){
+function decimalTwo(num) {
   return (Math.round(num * 100) / 100).toFixed(2);
 }
 let typeWriteCount = 0
@@ -78,4 +78,60 @@ function thousandDot(number) {
   //console.log(newNumberString)
   return newNumberString
 
+}
+
+let elem = document.documentElement;
+
+// let toggleFull = getid("toggleFull")
+
+let fullmode = false
+
+if (document.fullscreenElement) {
+  fullmode = true
+  //toggleFull.textContent = "No Full"
+}
+
+else {
+  //toggleFull.textContent = "Full"
+}
+
+function toggleFullscreen() {
+
+  console.log("clicked")
+
+  if (document.fullscreenElement) {
+    fullmode = true
+  } else { fullmode = false }
+
+  if (fullmode) {
+    closeFullscreen()
+    //toggleFull.textContent = "Full"
+  }
+  else {
+    console.log("no full")
+    openFullscreen()
+    //toggleFull.textContent = "No Full"
+
+  }
+
+}
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  console.log("activated")
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
 }
