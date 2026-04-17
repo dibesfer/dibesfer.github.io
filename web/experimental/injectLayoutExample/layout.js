823c610f9c37925378a62c4a818670e46e813a5c
dibesfer <dibesfer@gmail.com>
2026-04-17 21:38:31 +0200

    as

--- web/experimental/injectLayoutExample/layout.js
+++ web/experimental/injectLayoutExample/layout.js
@@ -1,10 +1,7 @@
 async function main() {
 
-  const header = await fetch("components/header.html").then(r => r.text());
-  const footer = await fetch("components/footer.html").then(r => r.text());
-
-  let html = header + footer
-
+  const html = await fetch("components/layout.html").then(r => r.text());
+  
   let layout = document.createElement("div")
   layout.id = "dibesfer_layout"
   layout.innerHTML = html
