ff37611ee9a557aa35850919151807f781e97429
dibesfer <dibesfer@gmail.com>
2026-05-14 17:31:11 +0200

    colorea

--- games/Kolorlando/KL3/assets/code/Wabavam/Voxel/12colors/12colors.js
+++ games/Kolorlando/KL3/assets/code/Wabavam/Voxel/12colors/12colors.js
@@ -5,15 +5,16 @@ export const voxelColors12 = [
     { id: 1, name: "White", color: "#ededed" },
     { id: 2, name: "Black", color: "#1f1f1f" },
     { id: 3, name: "Gray", color: "#8a8a8a" },
+    { id: 8, name: "Red", color: "#b42121" },
+    { id: 12, name: "Brown", color: "#583311" },
     { id: 4, name: "Orange", color: "#ff8e1e" },
     { id: 5, name: "Yellow", color: "#ebeb26" },
-    { id: 6, name: "LightBlue", color: "#3ad1ff" },
     { id: 7, name: "Green", color: "#169400" },
-    { id: 8, name: "Red", color: "#b42121" },
+    { id: 6, name: "LightBlue", color: "#3ad1ff" },
     { id: 9, name: "Blue", color: "#161de3" },
     { id: 10, name: "Purple", color: "#6b2cff" },
     { id: 11, name: "Pink", color: "#ff7eb6" },
-    { id: 12, name: "Brown", color: "#583311" },
+    
 ];
 
 export const voxelFakeShading12 = Object.freeze({
