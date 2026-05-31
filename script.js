29e94c935e072a879882516ae16cafd0d296a509
dibesfer <dibesfer@gmail.com>
2026-05-31 10:16:42 +0200

    ad

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-import { Memory } from "./assets/code/Memory.js";
-const memory = new Memory()
-
-hit(window.location.href).then((res) => {
-  const el = document.getElementById("visitsDisplay");
-  if (!el) return;
-
-  el.textContent =
-    typeof res.count === "number" ? res.count : "";
-});
-
-let localVisits = memory.load("dibesferV3_localVisits")
-
-desc.classList.toggle("invisible")
-
-if (!localVisits) {
-  localVisits = memory.save("dibesferV3_localVisits", 1)
-}
-
-else {
-  localVisits++
-  desc.innerHTML = "<b>Dib</b>ujos y <u>es</u>critos de <i>Fer</i>"
-  localVisits = memory.save("dibesferV3_localVisits", localVisits)
+// script.js
+const local = parseInt(localStorage.getItem("dibesferV3_localVisits")) || 0;
+desc.classList.remove("invisible");
+if (local > 1) {
+  desc.innerHTML = "<b>Dib</b>ujos y <u>es</u>critos de <i>Fer</i>";
 }
