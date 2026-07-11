e87e0299a367364bd43c0ccf1c65a67200dbff82
dibesfer <dibesfer@gmail.com>
2026-07-11 11:39:40 +0200

    push

---
+++ assets/pages/referror/script.js
@@ -0,0 +1,9 @@
+let listOfAudiovisuals = [
+
+    "https://upload.wikimedia.org/wikipedia/commons/7/7d/Lepsius-Projekt_tw_1-2-108.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/0/0e/Torii_path_with_lantern_at_Fushimi_Inari_Taisha_Shrine%2C_Kyoto%2C_Japan.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/c/c4/Al-Attarine_Madrasa_%288753601695%29.jpg"
+]
+
+
+audiovisuals.src = listOfAudiovisuals[randomInt(0,listOfAudiovisuals.length-1)]
